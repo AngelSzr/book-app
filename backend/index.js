e2b23fe03d2b47dd07530ea5ff0d4936b44d208d
Angel <angel.emilio@hotmail.com>
2023-06-27 21:53:08 -0600

    Second commit

--- backend/index.js
+++ backend/index.js
@@ -41,6 +41,30 @@ app.post("/books",(req,res)=>{
     })
 })
 
+app.delete('/books/:id',(req,res)=>{
+  const bookId=req.params.id
+  const q="DELETE FROM books WHERE id=?"
+
+  db.query(q,[bookId],(err,data)=>{
+    if(err) return res.json(err)
+    return res.json('Book has been deleted successfully.')
+  })
+})
+
+app.put('/books/:id',(req,res)=>{
+  const bookId=req.params.id
+  const q="UPDATE books SET `titles`=?, `description`=?, `price`=?,`cover`=? where id=?"
+
+  const values=[ req.body.titles,
+    req.body.description,
+    req.body.price,
+    req.body.cover,]  
+
+  db.query(q,[...values,bookId],(err,data)=>{
+    if(err) return res.json(err)
+    return res.json('Book has been updated successfully.')
+  })
+})
 
 app.listen(8800, () => {
   console.log("connected to BackEnd!");
